18f199708a56d9d9335db15d4a34496aed1c598c
SMXaS <Arnoldas.Jurkus@study.beds.ac.uk>
2019-03-12 15:16:43 +0000

    Test

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,6 +7,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import main.Entities.Customer;
 import main.Entities.Organization;
+import main.Entities.Price;
 import main.Entities.User;
 
 import java.util.Scanner;
@@ -29,7 +30,7 @@ public class Main extends Application {
 
 
     public static void main(String[] args) {
-        launch(args);
+        /*launch(args);
     }
 
     private static String getInput (String question) {
@@ -51,6 +52,74 @@ public class Main extends Application {
 
         userInput = (userInput.isEmpty() ? null : userInput);
         return userInput;
+
+   */
+
+
+
+        Scanner input = new Scanner(System.in);
+
+        Price pricePlace = new Price();
+
+        double price = pricePlace.getPrice();
+        String concertID = pricePlace.getConcertID();
+        String seat = pricePlace.getSeatRow();
+        int priceID = pricePlace.getPriceID();
+        double rate = pricePlace.getRate();
+        String name = pricePlace.getName();
+
+
+        System.out.println("Please Enter Concert Name: ");
+        while (input.hasNextLine()) {
+            name = input.nextLine();
+            break;
+        }
+
+        System.out.println("Please Enter Concert Price: ");
+        while (input.hasNextDouble()) {
+            price = input.nextDouble();
+            break;
+        }
+
+
+
+        System.out.println("Please Enter your Seat Number: ");
+        while (input.hasNext()) {
+            seat = input.next();
+            break;
+        }
+
+
+        System.out.println("Please Enter Concert ID: ");
+        while (input.hasNext()) {
+            concertID =input.next();
+            break;
+        }
+
+        System.out.println("Please Enter Price ID: ");
+        while (input.hasNext()) {
+            priceID = input.nextInt();
+            break;
+        }
+
+        System.out.println("Did you got discount? ");
+        while (input.hasNextDouble()) {
+            rate = input.nextDouble();
+            break;
+        }
+
+        if (!name.isEmpty() && price != -1 && !concertID.isEmpty() && rate != -1 && !seat.isEmpty() && priceID !=-1) {
+            //System.out.println("Error");
+        }
+        pricePlace.finalPrice(name, rate, priceID, seat, concertID, price);
+
+        System.out.println("Concert name is:  " + name);
+        System.out.println("Concert price is:  " + price + " £ ");
+        System.out.println("Your seat place is in : " + seat);
+        System.out.println("Concert ID is: " + concertID);
+        System.out.println("Price ID is: " + priceID);
+        System.out.println("Discount is: " + rate + " %");
+        System.out.println("Final price is : " + price / rate + " £ ");
     }
 
 }
